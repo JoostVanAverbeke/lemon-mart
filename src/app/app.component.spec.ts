059464fc65cf270bef65044a50ab5fc074bdae7f
Joost Van Averbeke <joost.van.averbeke@telenet.be>
2020-10-17 15:21:00 +0200

    Added common.testing module with fakes Fixed app component karma spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { commonTestingModules, DomSanitizerFake, MatIconRegistryFake, MediaObserverFake} from './common/common.testing';
+import { MediaObserver } from '@angular/flex-layout';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        commonTestingModules,
       ],
+      providers: commonTestingModules.concat([
+        { provide: MediaObserver, useClass: MediaObserverFake },
+        { provide: MatIconRegistry, useClass: MatIconRegistryFake },
+        { provide: DomSanitizer, useClass: DomSanitizerFake },
+      ]),
       declarations: [
         AppComponent
       ],
@@ -26,10 +35,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('lemon-mart');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('lemon-mart app is running!');
-  });
 });
