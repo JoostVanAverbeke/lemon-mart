40afde4e10ddc9881a1407d6a80c235dde3ffcfd
Joost Van Averbeke <joost.van.averbeke@telenet.be>
2020-10-17 11:18:21 +0200

    Added manager routing module. Added manager component containing the main toolbar

--- src/app/manager/manager-routing.module.ts
+++ src/app/manager/manager-routing.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ManagerHomeComponent } from './manager-home/manager-home.component';
+import { ManagerComponent } from './manager.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { ReceiptLookupComponent } from './receipt-lookup/receipt-lookup.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/manager/home', pathMatch: 'full'},
-  { path: 'home', component: ManagerHomeComponent },
+  { path: '', component: ManagerComponent,
+    children : [
+      { path: '', redirectTo: '/manager/home', pathMatch: 'full'},
+      { path: 'home', component: ManagerHomeComponent },
+      { path: 'users', component: UserManagementComponent },
+      { path: 'receipts', component: ReceiptLookupComponent },
+    ]
+  },
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
   exports: [RouterModule]
 })
 export class ManagerRoutingModule { }
