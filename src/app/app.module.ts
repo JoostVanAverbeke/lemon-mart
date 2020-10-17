fbaed962d5b6745fff958e8d42f46c6c742ea10e
Joost Van Averbeke <joost.van.averbeke@telenet.be>
2020-10-17 12:21:51 +0200

    Removed import of UserModule, PosModule and InventoryModule in app.module.ts Added styling of toolbar buttons in inventory component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { InventoryModule } from './inventory/inventory.module';
-import { PosModule } from './pos/pos.module';
-import { UserModule } from './user/user.module';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -26,9 +23,6 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     BrowserAnimationsModule,
     MaterialModule,
-    InventoryModule,
-    PosModule,
-    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
